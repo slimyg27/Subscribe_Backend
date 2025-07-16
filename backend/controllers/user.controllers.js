0fdd6f5024d25bdbb9d4d0c94e8065569d70eea4
Harshvardhan Dixit <slimyg07@gmail.com>
2025-07-16 16:23:27 +0530

    Added extra authorization for access user info

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -23,6 +23,12 @@ export const getSingleuser = async (req, res, next) => {
 
     try {
         
+        const userID = req.params.id;
+        const loggedin = req.user._id.toString();
+
+        if(userID != loggedin){
+            return res.status(401).json({message: "You're trying to access other user's info, PROHIBITTED"})
+        }
 
         const user = await User.findById(req.params.id).select('-password');
 
--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -17,7 +17,7 @@ const authorize = async (req,res,next) => {
     const decoded = jwt.verify(token, JWT_SECRET)
     const user = await User.findById(decoded.userId);
     if(!user)
-       return res.status(401).json({mesaage : 'Unauthorized', error : "wrong userId"})
+       return res.status(401).json({message : 'Unauthorized', error : "wrong userId"})
     
     req.user = user;
 
